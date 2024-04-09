16824f96902145cf9b3f260418aebb7cfc2e2049
Firas Brinsi <104305991+firasbrinsi1998@users.noreply.github.com>
2024-04-09 12:18:46 +0200

    Add optimized code approach

--- Easy/1 - TwoSum/TwoSum.py
+++ Easy/1 - TwoSum/TwoSum.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Apr  8 22:31:02 2024
+Created on Tue Apr  9 11:47:03 2024
 
 @author: Firas.Brinsi
 """
+"""
+#Brute force solution  
 
-class Solution(object):
+
+class Solution:
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
         if len(nums)>10**4 or len(nums)<=1:
             return 
         if target>10**9 or target<-10**9:
@@ -20,11 +18,31 @@ class Solution(object):
             for i in nums:
                 if i>10**9 or i<-10**9:
                     return 
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if nums[i]+nums[j]==target and i!=j:
-                    return (i,j)
+        
+        numbers={}
+        for i,number in enumerate(nums):
+            numbers[number]=i
+            a=target-number
+            if a in numbers and (numbers[a]!=numbers[number]):
+                return (numbers[a], numbers[number])
+            else:
+                numbers[number]=i
+            
+"""
+
+#Optimized code through dictionary data struct
+
+class Solution:
+    def twoSum(self, nums: List[int], target:int) -> List[int]:
+        dic=dict()
+        for i, number in enumerate(nums):
+            a=target-number
+            if a in dic:
+                return (i,dic[a])
+            dic[number]=i
+            
+            
 #Test case                
-#Solution().twoSum([3,3], 6)
-#Solution().twoSum([3,2,4],6)
+#print(Solution().twoSum([3,3], 6))
+#print(Solution().twoSum([3,2,4],6))
 
